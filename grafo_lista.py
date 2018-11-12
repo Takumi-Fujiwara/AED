628e2a85bc0522d6dd004f56d5768dc31c21d696
Lucas Rosa <lukaz.rosa@live.com>
2018-11-12 19:21:43 -0200

    Update grafo_lista.py
    Adicionada função busca em largura (distancia, menor caminho, nodo pai)

--- grafo_lista.py
+++ grafo_lista.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
+from collections import deque
 import os
-import time
 
 
 class Grafo(object):
@@ -9,6 +9,48 @@ class Grafo(object):
         self._grafo = defaultdict(set)
         self._direcionado = direcionado
         self.adiciona_arestas(arestas)
+
+
+    def busca_largura(self, u):
+        """ Realiza uma busca em largura, imprime a distancia em relação a u, imprime arvore menor caminho"""
+        cor = defaultdict(set)                  #Inicia Dicionarios Cor, Distancia e Pai
+        distancia = defaultdict(int)
+        pai = defaultdict(set)
+        arvore = defaultdict(set)
+
+        for x in list(self._grafo.keys()):      # Adiciona informações a todos os vertices, menos a raiz indicada u
+            if x == u:
+                continue
+            cor[x].add('B')
+            distancia[x] = 0
+            pai[x]
+
+        cor[u].add('G')                         # Adiciona informações da raiz u
+        distancia[u] = 0
+        pai[u]
+        fila = deque()
+        fila.append(u)
+
+        while fila:                             #Caso esteja vazia, retorna False
+            v = fila.popleft()
+            for y in self._grafo[v]:
+                if cor[y] == {'B'}:
+                    cor[y] = 'G'
+                    distancia[y] = distancia[v] + 1
+                    pai[y] = v
+                    fila.append(y)
+            cor[v] = 'P'
+
+        for y in pai:                           #Monta um dicionario da arvore primeiro na extensão
+            for x in pai[y]:                    #Arvore de menor caminho entre U e qualquer vertice
+                if x == set():
+                    continue
+                arvore[x].add(y)
+
+        print('Distancia do nodo', u, ':', distancia)
+        print('NODO / PAI:', pai)
+        print('Arvore em primeira extensão: ', arvore)
+        return
 
 
     def get_vertices(self):
@@ -25,11 +67,11 @@ class Grafo(object):
 
     def del_aresta(self,u,k):
         """Remove aresta entre u - k """
-        self._grafo[k].discard(u)
+        self._grafo[u].discard(k)
+
 
     def get_arestas(self):
         """ Retorna arestas do grafo """
-
         return [(k, v) for k in self._grafo.keys() for v in self._grafo[k]]
 
 
@@ -55,6 +97,7 @@ class Grafo(object):
     def conectado(self, u, v):
         """ Verifica se existe uma aresta entre u e v """
         return u in self._grafo and v in self._grafo[u]
+
 
     def fonte(self, u):
         """ Verifica se é uma Fonte: Retorna True/False """
@@ -97,7 +140,7 @@ class Grafo(object):
 
 
 #lista de arestas.
-arestas = [('A', 'B'), ('B', 'C'), ('A', 'C')]
+arestas = [('A', 'B'), ('B', 'C'), ('D', 'C'), ('A', 'D'), ('C', 'E')]
 
 # Grafo
 grafo = Grafo(arestas, direcionado=True)
@@ -115,33 +158,38 @@ while (m == True):
     print("9:sumidouro ")
     print("10:grau entrada ")
     print("11:grau saida ")
-    print("12:sair \n \n")
-    u = int(input('Digite sua opção: '))
+    print("12:Busca em largura ")
+    print("13:sair \n \n")
+
+    u = input('Digite sua opção: ')
+    if u == '':
+        os.system('cls');
+        continue
+    u = int(u)
+
 
     if (u ==1):
         os.system('cls');
         print(grafo._grafo)  # Imprimir grafo
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif(u ==2):
         os.system('cls');
         u = input('Digite o vertice a adicionar: ')
         Grafo.adiciona_vertice(grafo,u) #Adiciona vertice P
-        time.sleep(3)
         os.system('cls');
 
     elif(u ==3):
         os.system('cls');
-        u = input('Digite o vertice a deletar: ')
-        Grafo.del_vertice(grafo, u)  # Deleta u
-        time.sleep(3)
+        x = input('Digite o vertice a deletar: ')
+        Grafo.del_vertice(grafo, x)  # Deleta vertice x
         os.system('cls');
 
     elif (u ==4):
         os.system('cls');
         print(Grafo.get_vertices(grafo))  # Imprimi arestas'''
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif (u ==5):
@@ -149,7 +197,6 @@ while (m == True):
         x = input('Digite a 1 aresta: ')
         y = input('Digite a 2 aresta: ')
         Grafo.adiciona(grafo, x, y)  # Adiciona aresta entre u e v
-        time.sleep(3)
         os.system('cls');
 
     elif (u ==6):
@@ -157,42 +204,48 @@ while (m == True):
         x = input('Digite a 1 aresta: ')
         y = input('Digite a 2 aresta: ')
         Grafo.del_aresta(grafo, x, y) #Deleta aresta entre U x V
-        time.sleep(3)
         os.system('cls');
 
     elif (u ==7):
         os.system('cls');
         print(Grafo.get_arestas(grafo)) #Imprime as arestas
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif (u ==8):
         os.system('cls');
         x = input('Fonte: qual o vertices?')
         print(Grafo.fonte(grafo, x))  # Verifica se A é fonte
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif (u ==9):
         os.system('cls');
         x = input('Sumidouro: qual o vertices?')
         print(Grafo.sumidouro(grafo, x))  # Verifica se é sumidouro
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif (u ==10):
         os.system('cls');
         x = input('Grau de entrada: qual o vertice?')
         print(Grafo.grau_entrada(grafo, x))  # Verifica e imprime o grau de entrada
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif (u ==11):
         os.system('cls');
         x = input('Grau de saida: qual o vertice?')
         print(Grafo.grau_saida(grafo, x))  # Verifica e imprime o grau de saida
-        time.sleep(3)
+        input('pressione enter para continuar')
         os.system('cls');
 
     elif (u ==12):
+        os.system('cls');
+        x = input('Busca em largura: qual o vertice?')
+        Grafo.busca_largura(grafo,x)  # Verifica e imprime o grau de saida
+        input('pressione enter para continuar')
+        os.system('cls');
+
+    elif (u ==13):
         m = False
